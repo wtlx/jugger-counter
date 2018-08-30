f8617b7560168a3da2037cd550463537dbadfbd6
wtlx <35709516+wtlx@users.noreply.github.com>
2018-08-30 16:22:36 +0200

    Update jugger-counter_with_LED.ino
    Code for the randomly soldered Arduino Nano

--- jugger-counter_with_LED.ino
+++ jugger-counter_with_LED.ino
@@ -42,7 +42,7 @@ pinMode(ledPin, OUTPUT);      // Den LED-Pin zum Outpu-Pin machen
 
 // ab hier komtt der Code für das 7Seg-Display         
 byte numDigits = 4; //Hier wird die Anzahl der Ziffern angegeben
-byte digitPins[] = {2, 3, 4, 5}; //Die Pins zu den Ziffern werden festgelegt
+byte digitPins[] = {4, 3, 0, 1}; //Die Pins zu den Ziffern werden festgelegt
 byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13}; //Die Pins zu den Segmenten werden festgelegt
 sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins); 
 // je nach Displayart COMMON_CATHODE oder COMMON_ANODE eintragen
@@ -78,7 +78,7 @@ sevseg.refreshDisplay(); //hier nötig, da sonst die Zahl nur kurz blinkt
 
 sevseg.setNumber(zaehler); // Hier wird der Zählerstand an das Display übergeben...
 sevseg.refreshDisplay(); // ...und hier dafür gesorgt, dass er angezeigt wird
-sevseg.setBrightness(90); // Helligkeit von 0-100 
+sevseg.setBrightness(100); // Helligkeit von 0-100 
 
     set=1; // Hier beginnt die Magie des Entprellens
   }
@@ -89,13 +89,3 @@ sevseg.setBrightness(90); // Helligkeit von 0-100
   }
 ////////////////////////////////////////////////////////////////////////
 }
-
-
-
-
-
-
-
-
-
-
